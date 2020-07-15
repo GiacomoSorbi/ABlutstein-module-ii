8eabb5b2f93f195511d3ca0101648c040fb67eb7
Work H <aaronblutstein@Work-H.local>
2020-07-15 10:46:17 +0100

    Updates

---
+++ precache-manifest.804b9a07fd9782963887a39cd810ed1d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4824902adeceb47a665d2a04b07cd467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c679362b308f5c23c3a",
+    "url": "/static/css/main.74d6a7d7.chunk.css"
+  },
+  {
+    "revision": "7c503aebe5cf77d44f89",
+    "url": "/static/js/2.5256ed30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5256ed30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c679362b308f5c23c3a",
+    "url": "/static/js/main.fb2c49ae.chunk.js"
+  },
+  {
+    "revision": "6aa48d2c1d75ae2d562b",
+    "url": "/static/js/runtime-main.bec0f6b8.js"
+  },
+  {
+    "revision": "d315df3b2f4bd582e70787a3b45e88b8",
+    "url": "/static/media/Bing.d315df3b.jpg"
+  },
+  {
+    "revision": "1141a33b0961134617b6008221adba80",
+    "url": "/static/media/ButterflyKidsRegular.1141a33b.ttf"
+  },
+  {
+    "revision": "22ce3dc6a2c3f87b39f5bc8789257c44",
+    "url": "/static/media/LondrinaSketchRegular.22ce3dc6.ttf"
+  },
+  {
+    "revision": "b7d6b869c758b682a278921c4802f244",
+    "url": "/static/media/MyKids.b7d6b869.ttf"
+  },
+  {
+    "revision": "fbb2b4b9b7b52540a454d35289791c19",
+    "url": "/static/media/Rabbit-face.fbb2b4b9.png"
+  },
+  {
+    "revision": "e480751a2fb874d4b3505f83f1b94ef7",
+    "url": "/static/media/bookSearchIcon.e480751a.PNG"
+  },
+  {
+    "revision": "ec6ec0638ec249b6c2596f62e94e2a7b",
+    "url": "/static/media/defaultImage.ec6ec063.png"
+  },
+  {
+    "revision": "b7b778a666fd6217684600e00562e409",
+    "url": "/static/media/homePage_scenary.b7b778a6.png"
+  },
+  {
+    "revision": "3bdcf869706e740e47d069069b39f535",
+    "url": "/static/media/meetIcon.3bdcf869.PNG"
+  },
+  {
+    "revision": "e58069af9259bfe222a0a2a12c6c77ef",
+    "url": "/static/media/owlBlackboard.e58069af.png"
+  },
+  {
+    "revision": "0aa5de22006d1c14ec32948db2579781",
+    "url": "/static/media/owl_chair_final.0aa5de22.png"
+  },
+  {
+    "revision": "0edbb41d9663ed2ae3b71c776293c72c",
+    "url": "/static/media/paintIcon.0edbb41d.PNG"
+  },
+  {
+    "revision": "c2cbe92c334870838b9ae0e0a54cd5c5",
+    "url": "/static/media/pooh.c2cbe92c.jpg"
+  },
+  {
+    "revision": "40b0ba08c92096461b92d2d3637f4db6",
+    "url": "/static/media/rabbit_easel.40b0ba08.png"
+  },
+  {
+    "revision": "cc09b1046209c10e4c6daed592b24e94",
+    "url": "/static/media/toDoIcon.cc09b104.PNG"
+  },
+  {
+    "revision": "891a42081b46d6bd22c554dfdaa744dc",
+    "url": "/static/media/zog.891a4208.jpg"
+  }
+]);
